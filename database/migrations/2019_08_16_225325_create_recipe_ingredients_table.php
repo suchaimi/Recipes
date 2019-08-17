94d151841182d9dbb599acd98cec75dd5bfd0290
Suchaimi <suchaimi.afif@gmail.com>
2019-08-18 02:01:18 +0700

    Creating Model

--- database/migrations/2019_08_16_225325_create_recipe_ingredients_table.php
+++ database/migrations/2019_08_16_225325_create_recipe_ingredients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRecipeIngredientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('recipe_id')->unsigned();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('api-token')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('qty');
         });
     }
 
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipe_ingredients');
     }
 }
